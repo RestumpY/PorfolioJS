8a6fbdbd39227ee429e8413e324163f29e572157
RestumpY <yannis.lanista@hotmail.com>
2021-12-22 13:36:33 +0100

    JS

--- js/main.js
+++ js/main.js
@@ -75,3 +75,107 @@ imageMe.addEventListener('click', function () {
     
 
 })
+
+//My Skill on récupere les datas
+var skill1 = document.getElementById('skill1');
+var skill2 = document.getElementById('skill2');
+var skill3 = document.getElementById('skill3');
+var skill4 = document.getElementById('skill4');
+var skill5 = document.getElementById('skill5');
+var skill6 = document.getElementById('skill6');
+var skill7 = document.getElementById('skill7');
+var skill8 = document.getElementById('skill8');
+
+
+
+//Edit les datas de skill
+skill1.addEventListener('click',function(){
+    const newLangage = prompt ('Entrer votre langage');
+    const newPourcentage = prompt ('Entrer votre niveau');
+
+    if (newLangage.length > 0 && newPourcentage.length > 0 && newPourcentage <=100){
+        skill1.firstElementChild.firstElementChild.textContent = newLangage;
+        skill1.firstElementChild.lastElementChild.textContent = newPourcentage + "%";
+        skill1.lastElementChild.firstElementChild.style.width = newPourcentage + "%";
+    } 
+})
+
+skill2.addEventListener('click',function(){
+    const newLangage = prompt ('Entrer votre langage');
+    const newPourcentage = prompt ('Entrer votre niveau');
+
+    if (newLangage.length > 0 && newPourcentage.length > 0 && newPourcentage <=200){
+        skill2.firstElementChild.firstElementChild.textContent = newLangage;
+        skill2.firstElementChild.lastElementChild.textContent = newPourcentage + "%";
+        skill2.lastElementChild.firstElementChild.style.width = newPourcentage + "%";
+    }else{
+        skill2.removeEventListener();
+    }
+})
+
+skill3.addEventListener('click',function(){
+    const newLangage = prompt ('Entrer votre langage');
+    const newPourcentage = prompt ('Entrer votre niveau');
+
+    if (newLangage.length > 0 && newPourcentage.length > 0 && newPourcentage <=300){
+        skill3.firstElementChild.firstElementChild.textContent = newLangage;
+        skill3.firstElementChild.lastElementChild.textContent = newPourcentage + "%";
+        skill3.lastElementChild.firstElementChild.style.width = newPourcentage + "%";
+    } 
+})
+
+skill4.addEventListener('click',function(){
+    const newLangage = prompt ('Entrer votre langage');
+    const newPourcentage = prompt ('Entrer votre niveau');
+
+    if (newLangage.length > 0 && newPourcentage.length > 0 && newPourcentage <=400){
+        skill4.firstElementChild.firstElementChild.textContent = newLangage;
+        skill4.firstElementChild.lastElementChild.textContent = newPourcentage + "%";
+        skill4.lastElementChild.firstElementChild.style.width = newPourcentage + "%";
+    } 
+})
+
+skill5.addEventListener('click',function(){
+    const newLangage = prompt ('Entrer votre langage');
+    const newPourcentage = prompt ('Entrer votre niveau');
+
+    if (newLangage.length > 0 && newPourcentage.length > 0 && newPourcentage <=500){
+        skill5.firstElementChild.firstElementChild.textContent = newLangage;
+        skill5.firstElementChild.lastElementChild.textContent = newPourcentage + "%";
+        skill5.lastElementChild.firstElementChild.style.width = newPourcentage + "%";
+    } 
+})
+
+skill6.addEventListener('click',function(){
+    const newLangage = prompt ('Entrer votre langage');
+    const newPourcentage = prompt ('Entrer votre niveau');
+
+    if (newLangage.length > 0 && newPourcentage.length > 0 && newPourcentage <=600){
+        skill6.firstElementChild.firstElementChild.textContent = newLangage;
+        skill6.firstElementChild.lastElementChild.textContent = newPourcentage + "%";
+        skill6.lastElementChild.firstElementChild.style.width = newPourcentage + "%";
+    } 
+})
+
+skill7.addEventListener('click',function(){
+    const newLangage = prompt ('Entrer votre langage');
+    const newPourcentage = prompt ('Entrer votre niveau');
+
+    if (newLangage.length > 0 && newPourcentage.length > 0 && newPourcentage <=700){
+        skill7.firstElementChild.firstElementChild.textContent = newLangage;
+        skill7.firstElementChild.lastElementChild.textContent = newPourcentage + "%";
+        skill7.lastElementChild.firstElementChild.style.width = newPourcentage + "%";
+    } 
+})
+
+skill8.addEventListener('click',function(){
+    const newLangage = prompt ('Entrer votre langage');
+    const newPourcentage = prompt ('Entrer votre niveau');
+
+    if (newLangage.length > 0 && newPourcentage.length > 0 && newPourcentage <=800){
+        skill8.firstElementChild.firstElementChild.textContent = newLangage;
+        skill8.firstElementChild.lastElementChild.textContent = newPourcentage + "%";
+        skill2.lastElementChild.firstElementChild.style.width = newPourcentage + "%";
+    } 
+})
+
